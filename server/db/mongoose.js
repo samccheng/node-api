1a2764212c4cf5c817ba50eb1e022701cbfcd819
Sam Cheng <samcheng8@gmail.com>
2018-01-05 19:42:58 -0700

    fix mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_ORANGE_URI || 'mongodb://localhost/Todos');
+mongoose.connect(process.env.MONGOLAB_ORANGE_URI || 'mongodb://localhost/Todos');
 mongoose.Promise = global.Promise;
 
 module.exports = { mongoose }
