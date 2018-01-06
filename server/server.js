019d34e9fab30eb7928e9aeff3767e9b868775de
Sam Cheng <samcheng8@gmail.com>
2018-01-05 20:33:17 -0700

    add delete route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,24 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send())
 })
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then(doc => {
+    if(!doc) {
+      res.status(404).send()
+    }
+    res.status(200).send()
+    
+  }).catch( e => res.status(400).send())
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
